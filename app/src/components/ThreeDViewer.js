d16806eb101e94101f954d83dc1ba66a93a5b0bb
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-08-17 11:04:27 -0700

    Add starfield background for immersive feel

--- app/src/components/ThreeDViewer.js
+++ app/src/components/ThreeDViewer.js
@@ -56,6 +56,19 @@ const ThreeDViewer = () => {
         
         scene.add(satelliteGroup);
 
+        const starVertices = [];
+        for (let i = 0; i < 10000; i++) {
+            const x = (Math.random() - 0.5) * 2000;
+            const y = (Math.random() - 0.5) * 2000;
+            const z = (Math.random() - 0.5) * 2000;
+            starVertices.push(x, y, z);
+        }
+        const starGeometry = new THREE.BufferGeometry();
+        starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+        const starMaterial = new THREE.PointsMaterial({ color: 0x888888, size: 0.7 });
+        const stars = new THREE.Points(starGeometry, starMaterial);
+        scene.add(stars);
+
         let isDragging = false;
         let previousMousePosition = { x: 0, y: 0 };
 
