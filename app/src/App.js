ece967f5b04e01a640189c41b32148cbbba80718
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-08-17 11:21:40 -0700

    Build main layout and HUD components

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ThreeDViewer from './components/ThreeDViewer';
+import HudElement from './components/Hud';
 import './App.css';
 
 export default function App() {
   return (
     <main className='relative w-screen h-screen bg-[#0a192f]'>
+      <HudElement position="top-left" />
+      <HudElement position="top-right" />
+      <HudElement position="bototm-left" />
+      <HudElement position="bottom-right" />
+      <header className='fixed top-0 left-0 w-full p-4 md:p-8 z-10 pointer-events-none'>
+        <div className='text-lf font-bold text-teal-300 tracking-widest'>
+          GREESH VARTHAN <span className='font-light text-white'>ANUSHA SINGARAVELAN</span>
+        </div>
+      </header>
       <div className='absolute top-0 left-0 w-full h-full'>
         <ThreeDViewer />
       </div>
---
+++ app/src/components/Hud.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HudElement = ({ position }) => {
+    const baseStyle = "fixed w-8 h-8 border-teal-300/40 pointer-events-none";
+    const positions = {
+        'top-left': 'top-4 left-4 border-t-2 border-l-2',
+        'top-right': 'top-4 right-4 border-t-2 border-r-2',
+        'bottom-left': 'bottom-4 left-4 border-b-2 border-l-2',
+        'bottom-right': 'bottom-4 right-4 border-b-2 border-r-2'
+    };
+
+    return <div className={`${baseStyle} ${positions[position]}`}></div>;
+}
+
+export default HudElement;
