1a9fc2fed116d8cf6a570a0e7ce97a4bbeab0229
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-11-13 15:01:44 -0800

    Extract data into separate files for efficient management

---
+++ app/src/data/skillsData.js
@@ -0,0 +1,5 @@
+export const skillsData = {
+  languages: ["Python", "C", "C++", "MATLAB", "Java"],
+  software: ["SolidWorks", "CREO", "CATIA", "ANSYS", "StarCCM+", "MSC Patran", "Abaqus CAE"],
+  concepts: ["Orbital Mechanics", "CFD", "FEA", "Propulsion", "Structure Mechanics", "Composite Materials"]
+};
