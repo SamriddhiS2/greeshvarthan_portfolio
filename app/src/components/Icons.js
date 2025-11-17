9ed828f5e362788b815f6c00ce947b6f03379f83
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-11-16 18:23:46 -0800

    Add an airplane icon

--- app/src/components/Icons.js
+++ app/src/components/Icons.js
@@ -30,3 +30,20 @@ export const InstagramIcon = () => (
 export const ArrowLeftIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
 );
+
+export const AirplaneIcon = ({ className }) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
+    className={className}
+    style={{ width: '1.25em', height: '1.25em', display: 'inline-block' }}
+  >
+    <path d="M22 2L11 13" />
+    <path d="M22 2L15 22L11 13L2 9L22 2Z" />
+  </svg>
+);
