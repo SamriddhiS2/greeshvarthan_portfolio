9168d09dbbd3eb136213f6b4760ebcab00223ed7
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-11-16 18:26:58 -0800

    Create a globe transition component

---
+++ app/src/components/GlobeTransition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GlobeTransition = () => (
+  <div className="globe-transition">
+    <div className="relative flex justify-center items-center">
+      <div className="globe"></div>
+    </div>
+  </div>
+);
+
+export default GlobeTransition;
